d6235a13dfbb5c1fc8621a5d132803059c786bf4
DimaSwiftYandex <otlichnik24@yandex.ru>
2023-10-04 14:32:05 +0300

    adjusted cornerRadius and borderWidth

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -61,20 +61,18 @@ final class MovieQuizViewController: UIViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        //        let currentQuestion = questions[currentQuestionIndex]
+        imageView.layer.cornerRadius = 20
         let currentScreen = convert(model: currentQuestion)
         show(quiz: currentScreen)
     }
     
     @IBAction private func yesButtonTapped(_ sender: UIButton) {
-        //        let currentQuestion = questions[currentQuestionIndex]
         currentQuestion.correctAnswer 
         ? showAnswerResult(isCorrect: true)
         : showAnswerResult(isCorrect: false)
     }
     
     @IBAction private func noButtonTapped(_ sender: UIButton) {
-        //        let currentQuestion = questions[currentQuestionIndex]
         currentQuestion.correctAnswer
         ? showAnswerResult(isCorrect: false)
         : showAnswerResult(isCorrect: true)
@@ -82,8 +80,8 @@ final class MovieQuizViewController: UIViewController {
     
     private func showAnswerResult(isCorrect: Bool) {
         imageView.layer.masksToBounds = true
-        imageView.layer.borderWidth = 1
-        imageView.layer.cornerRadius = 6
+            imageView.layer.borderWidth = 8
+            imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
         
         if isCorrect {
             imageView.layer.borderColor = UIColor.ypGreen.cgColor
@@ -137,7 +135,6 @@ final class MovieQuizViewController: UIViewController {
             preferredStyle: .alert
         )
     
-    
         let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
@@ -167,71 +164,3 @@ struct QuizResultsViewModel {
   let text: String
   let buttonText: String
 }
-
-
-
-
-
-/*
- Mock-данные
- 
- 
- Картинка: The Godfather
- Настоящий рейтинг: 9,2
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Dark Knight
- Настоящий рейтинг: 9
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Kill Bill
- Настоящий рейтинг: 8,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Avengers
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Deadpool
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Green Knight
- Настоящий рейтинг: 6,6
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Old
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: The Ice Age Adventures of Buck Wild
- Настоящий рейтинг: 4,3
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: Tesla
- Настоящий рейтинг: 5,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: Vivarium
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- */
